04431702036732a2db51e8a9ea05c595fad48ff6
Dearian <dearian2706@gmail.com>
2022-09-27 22:07:10 +0300

    input ref on typescript

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './styles.css'
 
 
@@ -9,14 +9,20 @@ interface Props {
 }
 
 const InputField:React.FC<Props>= ({todo, setTodo, handleAdd}) => {
-  // const inputRef = useR
+  const inputRef = useRef<HTMLInputElement>(null)
 
   return(
-    <form className='input' onSubmit={handleAdd}>
+    <form 
+    className='input' 
+    onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur()
+    }}>
       <input type="input"
+      ref={inputRef}
       value={todo}
       onChange={(e) => setTodo(e.target.value)}
-      placeholder='Enter a task' 
+      placeholder='Enter a task'
       className='inputBox' />
       <button className='btnSubmit' type='submit'>Add</button>
     </form>
