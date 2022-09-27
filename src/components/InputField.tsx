f773c534c3262080f2fbde45972a36e33797fc3e
Dearian <dearian2706@gmail.com>
2022-09-27 21:20:19 +0300

    lay out; input functional

---
+++ src/components/InputField.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles.css'
+
+
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e: React.FormEvent) => void,
+}
+
+const InputField:React.FC<Props>= ({todo, setTodo, handleAdd}) => {
+  // const inputRef = useR
+
+  return(
+    <form className='input' onSubmit={handleAdd}>
+      <input type="input"
+      value={todo}
+      onChange={(e) => setTodo(e.target.value)}
+      placeholder='Enter a task' 
+      className='inputBox' />
+      <button className='btnSubmit' type='submit'>Add</button>
+    </form>
+  )
+}
+
+export default InputField
